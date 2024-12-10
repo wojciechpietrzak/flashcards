8a0d11c8096c842eef1d01f46e9faced1d604867
Wojciech Pietrzak <wojciech.pietrzak@omnibus.edu.pl>
2024-12-10 19:25:35 +0100

    feat: German translations for password.php

--- lang/de/passwords.php
+++ lang/de/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Ihr Passwort wurde zurückgesetzt.',
+    'sent' => 'Wir haben Ihnen den Link zum Zurücksetzen des Passworts geschickt.',
+    'throttled' => 'Warten Sie bitte, bevor Sie es erneut versuchen.',
+    'token' => 'Dieses Token zum Zurücksetzen des Passworts ist nicht korrekt.',
+    'user' => "Wir können den Benutzer mit dieser Email nicht finden.",
 
 ];
