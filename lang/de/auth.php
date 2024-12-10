485fa48e7e0a764e1bc0a29f959f8252ca25ce58
Wojciech Pietrzak <wojciech.pietrzak@omnibus.edu.pl>
2024-12-10 19:19:26 +0100

    feat: German translations for auth.php

--- lang/de/auth.php
+++ lang/de/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Diese Anmeldeinformationen sind nicht korrekt.',
+    'password' => 'Dieses Passwort ist nicht korrekt.',
+    'throttle' => 'Zu viele Einloggenversuche. Warten Sie bitte :seconds Sek. bevor Sie sich noch einmal einloggen versuchen.',
 
 ];
